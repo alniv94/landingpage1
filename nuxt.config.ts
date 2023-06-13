2af2e951812234c234a15374c7b9681fd81207d1
alniv1234 <alvinmiracastillo@gmail.com>
2023-06-13 17:08:16 +0800

    config for nuxt extend feat

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  // build: {
-  //   transpile: ["vuetify"],
-  // },
-  // modules: ["nuxt-icon"],
+  build: {
+    transpile: ["vuetify"],
+  },
+  modules: ["nuxt-icon"],
 });
